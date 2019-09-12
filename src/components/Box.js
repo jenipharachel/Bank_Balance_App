0425c243b42c706934faea73841eb5b17418612c
jenipha <jenipharachel@gmail.com>
2019-09-12 22:03:57 +0530

    money is added to state

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,18 @@
 import React from "react";
-import store from "./store";
+import { createStore } from "redux";
+
+const reducer = (state = 100000, action) => {
+  switch (action.type) {
+    case "inc":
+      return state + 10000;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const incBal = () => {
   return {
@@ -12,7 +25,12 @@ class Box extends React.Component {
     return (
       <div className="border border box-container">
         <p>Your balance is: {store.getState()}</p>
-        <button className="btn btn-default" onClick={store.dispatch(incBal())}>
+        <button
+          className="btn btn-default"
+          onClick={() => {
+            store.dispatch(incBal());
+          }}
+        >
           Increment
         </button>
         <button className="btn btn-default">Decrement</button>
--- src/components/store.js
+++ src/components/store.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 
 const reducer = (state = 100000, action) => {
   switch (action.type) {
+    case "inc": return state + 10000;
     default:
       return state;
   }
