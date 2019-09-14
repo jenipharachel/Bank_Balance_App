0d70a7e799634973bc12aeb4844b741591bfc9f9
jenipha <jenipharachel@gmail.com>
2019-09-14 13:29:45 +0530

    changes deployed to prod

---
+++ build/precache-manifest.f7514edf4e80887cdb4e0dd467e36cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c940f21a654cf81f7d8532403112262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8634adfb784349ea4114",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e71456e5b649f803c931",
+    "url": "/static/css/main.82accea5.chunk.css"
+  },
+  {
+    "revision": "8634adfb784349ea4114",
+    "url": "/static/js/2.dde4c7a1.chunk.js"
+  },
+  {
+    "revision": "e71456e5b649f803c931",
+    "url": "/static/js/main.3b8ace60.chunk.js"
+  },
+  {
+    "revision": "d59a6bd8d4905f06dff5",
+    "url": "/static/js/runtime~main.63033c61.js"
+  }
+]);
